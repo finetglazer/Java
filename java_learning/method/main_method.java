52b984417cb38d80de4b33d9aeaf8d6c31db9268
I hate iu <101011776+finetglazer@users.noreply.github.com>
2023-02-20 23:39:33 +0700

    Create main_method.java

---
+++ java_learning/method/main_method.java
@@ -0,0 +1,8 @@
+public class main_method {
+    public static void main(String[] args)
+    the keyword public indicates that the method can be invoked from everywhere;
+    the keyword static indicates the method can be invoked without creating an instance of the class;//ko can goi ten
+    the keyword void indicates the method doesn't return any value;
+    the array variable args contains arguments entered at the command line,
+        the array is empty if there are no arguments.
+}
