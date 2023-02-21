49b9853e1e176c9071d9afdec304ef481696eda1
I hate iu <101011776+finetglazer@users.noreply.github.com>
2023-02-21 22:58:07 +0700

    new

---
+++ java_learning/data_type_and_var/final_var.java
@@ -0,0 +1,11 @@
+public class final_var {
+    final var is constant var 
+    it must be assigned before print 
+    2. final reference
+    Note, that it is always possible to change the internal state of an object pointed at by a final reference variable, i.e.
+     the constant is only the variable itself (the reference), not the object to which it refers.
+     khi thay doi tham chieu ma no tro den thi thay the the internal state.
+     final StringBuilder builder = new StringBuilder(); // ""
+    builder.append("Hello!"); // it works
+    System.out.println(builder.toString()); // Hello!
+}
